6d97dc524fab549c44aead0f5de0a20219e26688
Hossein Aghatabbar MacBook <whossein@hotmail.com>
2023-03-07 18:11:21 +0330

    remove private code

--- config/constant.ts
+++ config/constant.ts
@@ -4,16 +4,14 @@ const devModes = {
 };
 export const isTestEnv = true;
 
-export const botToken = "6291644750:AAFBLJOkGRYq4o44pt1fiRfvXMQWbfwX7dw";
+export const botToken = "";
 
 export const inLocal = true;
 export const needTelegramBot = false;
 
 export const storagePath = inLocal ? "./x-ui.db" : "/etc/x-ui/x-ui.db";
 
-export const ExampleTrojan =
-  "trojan://pNoxrWqnxn@tel.netbros.ir:55684?type=tcp&security=tls#test-hos";
-export const ExampleVless =
-  "vless://7ab49239-46e9-45b9-8fd2-42ab0e1a327b@tel.netbros.ir:30127?type=tcp&security=tls&fp=chrome#vless-test";
+export const ExampleTrojan = "trojan://";
+export const ExampleVless = "vless://";
 
 export const port = 733;
