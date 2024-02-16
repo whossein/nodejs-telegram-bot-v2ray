f26e1318a75f084aef0a65215d73554f1b7d01ef
Hossein Aghatabar <whossein@hotmail.com>
2024-02-16 12:11:35 +0330

    Update constant.ts

--- config/constant.ts
+++ config/constant.ts
@@ -4,7 +4,7 @@ const devModes = {
 };
 export const isTestEnv = true;
 
-export const botToken = "6291644750:AAFBLJkOkGRYq4o44pt1fiRfvXMQWbfw-X7dw";
+export const botToken = "";
 
 export const inLocal = true;
 export const needTelegramBot = false;
